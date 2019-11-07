bb3f39498ad9d825816be9d503ba59b3bd44788c
Zongshi Liu <uceszl9@ucl.ac.uk>
2019-11-07 19:39:50 +0000

    Add comment and try to improve grogram

--- main_from_user.py
+++ main_from_user.py
@@ -57,9 +57,11 @@ class Polygon(Geometry):
 
 
 class MBR(Polygon):
+    # The Minimum Bounding Rectangle Algorithm.
     def __init__(self, name, polylist):
         length = len(polylist)
         top = down = left = right = polylist[0]
+        # Get four vertices of the MBR.
         for i in range(1, length):
             if polylist[i].get_x() > top.get_x():
                 top = polylist[i]
@@ -81,7 +83,9 @@ class MBR(Polygon):
         return mbr
 
 
+# The Point-in-Polygon Algorithm.
 class Judgement:
+    # Determine whether the point is inside the MBR.
     def is_point_in_mbr(self, point, mbr):
         if mbr[3].get_x() <= point.get_x() <= mbr[0].get_x() and \
                 mbr[3].get_y() <= point.get_y() <= mbr[2].get_y():
@@ -89,14 +93,18 @@ class Judgement:
         else:
             return False
 
+    # Determine whether the point set is inside the polygon.
+    # Return a point set that each point have "category" attribute.
     def pip_test(self, pointlist, polylist):
         mbr = MBR("polygon", polylist).get_mbr()
         pointlist_aftertest = []
         for point in pointlist:
+            # Determine whether the point is inside the MBR.
             if not self.is_point_in_mbr(point, mbr):
                 point.define_category("outside")
                 pointlist_aftertest.append(point)
                 continue
+            # The Ray Casting Algorithm.
             lines = Polygon("polygonlines", polylist).lines()
             p_x = point.get_x()
             p_y = point.get_y()
@@ -106,21 +114,27 @@ class Judgement:
                 p1_y = line.point_1.get_y()
                 p2_x = line.point_2.get_x()
                 p2_y = line.point_2.get_y()
+                # Point coincides with the vertices of the polygon.
                 if (p_x == p1_x and p_y == p1_y) or (p_x == p2_x and p_y == p2_y):
                     point.define_category("boundary")
                     break
+                # Point is on the boundaries that are parallel with X axis.
                 if p1_y == p_y == p2_y:
                     if (p1_x < p_x < p2_x) or (p2_x < p_x < p1_x):
                         point.define_category("boundary")
                         break
+                # Determine whether the end points of the line segment are on either side of the ray.
                 if (p1_y < p_y <= p2_y) or (p2_y < p_y <= p1_y):
-                    x = p2_x - (p2_y - p_y) * \
-                        (p2_x - p1_x) / (p2_y - p1_y)
+                    # The x-coordinate of a point on a line segment that has same y-coordinate with the ray.
+                    x = p2_x - (p2_y - p_y) * (p2_x - p1_x) / (p2_y - p1_y)
+                    # Point is on the boundary of the polygon.
                     if x == p_x:
                         point.define_category("boundary")
                         break
+                    # The ray pass through the boundary of the polygon.
                     if x > p_x:
                         counting += 1
+            # Determine whether the number of intersections is even.
             if point.get_category() != "boundary":
                 if counting % 2 == 0:
                     point.define_category("outside")
@@ -131,21 +145,51 @@ class Judgement:
 
 
 class File:
+    # Read file by using "DictReader" function, return point set.
     def read_file(self, filename=""):
-        pointlist = []
-        with open(filename, "r") as f:  # Read file by using "DictReader" function
-            reader = csv.DictReader(f)
-            for row in reader:
-                x = float(row["x"])
-                y = float(row["y"])
-                id_num = row["id"]
-                point = Point(id_num, x, y)
-                pointlist.append(point)
+        with open(filename, "r") as f:
+            pointlist = []
+            for i in f.readlines()[1:]:
+                point = i.split(",")
+                id = point[0]
+                x = float(point[1])
+                y = float(point[2])
+                point1 = Point(id, x, y)
+                pointlist.append(point1)
         return pointlist
 
-    def print_on_screen(self, pointlist, polylist):
-        points_aftertest = Judgement().pip_test(pointlist, polylist)
-        for i in points_aftertest:
+
+    # Read tuple_pairs given by user, return point set.
+    def read_from_user(self, tuple_pairs):
+        # Use eval() function to extract point set given by user.
+        # (Source: Programiz. 2019. https://www.programiz.com/python-programming/methods/built-in/eval)
+        points = eval(tuple_pairs)
+        # If user type in wrong way, ask user type again.
+        # (Source: Programiz. 2019. https://www.programiz.com/python-programming/methods/built-in/isinstance)
+        while isinstance(points, tuple) is False:
+            print("Type in wrong way! Please try again!")
+            points = eval(input("Please enter the points: "))
+        pointslist = []
+        # Get point set if user type more than one point.
+        if isinstance(points[0], tuple):
+            for i in range(len(points)):
+                id_num = i
+                x = float(points[i][0])
+                y = float(points[i][1])
+                point = Point(id_num, x, y)
+                pointslist.append(point)
+        # Get point set if user type only one point.
+        else:
+            id_num = 0
+            x = float(points[0])
+            y = float(points[1])
+            point = Point(id_num, x, y)
+            pointslist.append(point)
+        return pointslist
+
+    # Print result on screen.
+    def print_on_screen(self, point_aftertest):
+        for i in point_aftertest:
             point = (i.get_x(), i.get_y())
             category = i.get_category()
             if category == "outside":
@@ -157,6 +201,7 @@ class File:
 
 
 class Extractor:
+    # Extract x-coordinates and y-coordinates of the polygon.
     def extract_polygon(self, polylist, name):
         x_polygon = []
         y_polygon = []
@@ -168,15 +213,16 @@ class Extractor:
         if name == "ys":
             return y_polygon
 
-    def extract_point(self, point_set, polygon_set, category, name):
-        points_aftertest = Judgement().pip_test(point_set, polygon_set)
+    # Extract x-coordinates and y-coordinates of points after PiP test.
+    # Return coordinates based on category of each point.
+    def extract_point(self, point_aftertest, category, name):
         x_outside_list = []
         y_outside_list = []
         x_inside_list = []
         y_inside_list = []
         x_boundary_list = []
         y_boundary_list = []
-        for i in points_aftertest:
+        for i in point_aftertest:
             if i.get_category() == "outside":
                 x_outside_list.append(i.get_x())
                 y_outside_list.append(i.get_y())
@@ -200,31 +246,39 @@ class Extractor:
             return y_boundary_list
 
 
+# Run the Point-in-Polygon Test and plot polygon and points.
+# Pass a tuple pairs given by user as an argument to the function.
 def main(tuple_pairs):
     plotter = Plotter()
 
     print("read polygon.csv")
     file = File()
     polygon_set = file.read_file("polygon.csv")
+
     print("Insert point information")
-    tuple_pairs
+    point_set = file.read_from_user(tuple_pairs)
+
     print("categorize points")
-    file.print_on_screen(tuple_pairs, polygon_set)
+    judge = Judgement()
+    point_aftertest = judge.pip_test(point_set, polygon_set)
+    file.print_on_screen(point_aftertest)
+
     print("plot polygon and points")
     x_polygon = Extractor().extract_polygon(polygon_set, "xs")
     y_polygon = Extractor().extract_polygon(polygon_set, "ys")
     plotter.add_polygon(x_polygon, y_polygon)
 
-    x_outside_list = Extractor().extract_point(tuple_pairs, polygon_set, "outside", "xs")
-    y_outside_list = Extractor().extract_point(tuple_pairs, polygon_set, "outside", "ys")
+    # Plot points based on category of each point
+    x_outside_list = Extractor().extract_point(point_aftertest, "outside", "xs")
+    y_outside_list = Extractor().extract_point(point_aftertest, "outside", "ys")
     plotter.add_point(x_outside_list, y_outside_list, "outside")
 
-    x_inside_list = Extractor().extract_point(tuple_pairs, polygon_set, "inside", "xs")
-    y_inside_list = Extractor().extract_point(tuple_pairs, polygon_set, "inside", "ys")
+    x_inside_list = Extractor().extract_point(point_aftertest, "inside", "xs")
+    y_inside_list = Extractor().extract_point(point_aftertest, "inside", "ys")
     plotter.add_point(x_inside_list, y_inside_list, "inside")
 
-    x_boundary_list = Extractor().extract_point(tuple_pairs, polygon_set, "boundary", "xs")
-    y_boundary_list = Extractor().extract_point(tuple_pairs, polygon_set, "boundary", "ys")
+    x_boundary_list = Extractor().extract_point(point_aftertest, "boundary", "xs")
+    y_boundary_list = Extractor().extract_point(point_aftertest, "boundary", "ys")
     plotter.add_point(x_boundary_list, y_boundary_list, "boundary")
 
     plotter.show()
@@ -238,24 +292,5 @@ if __name__ == "__main__":
           "point(x1, y1): outside the polygon.\n"
           "point(x2, y2): inside the polygon.\n"
           "point(x3, y3): on the boundary.\n")
-    points = eval(input("Please enter the points: "))
-    while isinstance(points, tuple) is False:
-        print("Type in wrong way! Please try again!")
-        points = eval(input("Please enter the points: "))
-    pointslist = []
-    if isinstance(points[0], tuple):
-        for i in range(len(points)):
-            id_num = i
-            x = float(points[i][0])
-            y = float(points[i][1])
-            point = Point(id_num, x, y)
-            pointslist.append(point)
-    else:
-        id_num = 0
-        x = float(points[0])
-        y = float(points[1])
-        point = Point(id_num, x, y)
-        pointslist.append(point)
-    tuple_pairs = pointslist
 
-    main(tuple_pairs)
+    main(input("Please enter the points: "))
